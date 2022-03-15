c0e8763fe5e9e0fa77ece396e03176f3db9f5e6e
vitkynanda <vitkynptr@gmail.com>
2022-03-15 21:28:41 +0700

    fix get detail product

--- user-backend/repository/product_repository/product_repository_impl.go
+++ user-backend/repository/product_repository/product_repository_impl.go
@@ -23,16 +23,37 @@ func (repo *productRepository) GetAllProducts() ([]entity.Product, error) {
 }
 
 func (repo *productRepository) GetProductById(id string) (*entity.ProductDetail, error) {
-	product := entity.ProductDetail{}
-	
-	// err := repo.mysqlConnection.Model(&entity.Product{}).Where("id = ?", id).Find(&product).Error
-	
-	err := repo.mysqlConnection.Model(&entity.Product{}).Where("products.id = ?", id).Select("products.name, products.status,  products.id, products.description").Joins("left join users on users.id = products.checker_id and users.id = products.signer_id and users.id = products.maker_id").Scan(&product).Error
+	productDetail := entity.ProductDetail{}
+	product := entity.Product{}
+	users := []entity.User{}
+
+	if err := repo.mysqlConnection.Where("id = ?", id).Find(&product).Error; err != nil {
+		return nil, err
+	}
+
+	productDetail.ID = product.ID
+	productDetail.Name = product.Name
+	productDetail.Description = product.Description
+	productDetail.Status = product.Status 
+
+	sql := "SELECT * FROM users WHERE id IN"
+	sql = fmt.Sprintf("%s ('%s' ,'%s' ,'%s')", sql, product.MakerID, product.CheckerID, product.MakerID)
+
+	err :=  repo.mysqlConnection.Raw(sql).Scan(&users).Error;
 	if err != nil {
 		return nil, err
 	}
 
-	return &product, nil
+	if (len(users)>0){
+		productDetail.MakerID = users[0].ID
+		productDetail.MakerName = users[0].Name
+		productDetail.CheckerID = users[1].ID
+		productDetail.CheckerName = users[1].Name
+		productDetail.SignerID = users[2].ID
+		productDetail.SignerName = users[2].Name
+	}
+	
+	return &productDetail, nil
 }
 
 func (repo *productRepository) CreateNewProduct(product entity.Product) (*entity.Product,  error){
